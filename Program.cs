82e271156ff9133af5e0c5b8f091d0dba51c8ceb
Philip Dryden <dryden.github@posteo.de>
2026-08-17 18:56:31 +0200

    Test File operations

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿using Google.GenAI;
 
+string input = File.ReadAllText("input.txt");
+
 var client = new Client();
 
-var result = await client.Models.GenerateContentAsync("gemini-3.1-flash-lite" , "Was ist 2 + 2?");
+var result = await client.Models.GenerateContentAsync("gemini-3.1-flash-lite" , input);
+
+File.WriteAllText("result.txt", result.Text);
 
 Console.WriteLine(result.Text);
