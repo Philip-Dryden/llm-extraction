6eb9ddab5ca7b8bf337f4a94243edb4b586fce9a
Philip Dryden <dryden.github@posteo.de>
2026-08-17 18:35:41 +0200

    Test Gemini API request

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Google.GenAI;
+
+var client = new Client();
+
+var result = await client.Models.GenerateContentAsync("gemini-3.1-flash-lite" , "Was ist 2 + 2?");
+
+Console.WriteLine(result.Text);
